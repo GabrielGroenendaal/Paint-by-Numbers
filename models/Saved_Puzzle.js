b84b88f1ad539d549bd98f9134524f2aa1df21ff
miker704 <miker704@gmail.com>
2022-07-18 12:48:02 -0400

    added redux tools and modded store,js

--- models/Saved_Puzzle.js
+++ models/Saved_Puzzle.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const SavedPuzzleSchema = new Schema({});
+const SavedPuzzleSchema = new Schema({
+    
+});
 
 const SavedPuzzle = mongoose.model('saved_puzzles', SavedPuzzleSchema);
 module.exports = SavedPuzzle;
