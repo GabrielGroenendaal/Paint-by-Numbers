d51c506b5542f5b0969593c07c660bd3a4e826bd
miker704 <miker704@gmail.com>
2022-07-18 21:34:20 -0400

    changed != to !==

--- frontend/src/components/play_puzzle/tile.jsx
+++ frontend/src/components/play_puzzle/tile.jsx
@@ -49,7 +49,7 @@ class Tile extends React.Component {
                         classText += " tile-unexplored"
             }
             if (mouseOverTile) {
-                  if (renderTile.pos != mouseOverTile.pos && (renderTile.pos[0] === mouseOverTile.pos[0] || renderTile.pos[1] === mouseOverTile.pos[1])) {
+                  if (renderTile.pos !== mouseOverTile.pos && (renderTile.pos[0] === mouseOverTile.pos[0] || renderTile.pos[1] === mouseOverTile.pos[1])) {
                         //classText += ' tile-highlighted'
                         overlayClassText += ' tile-highlighted'
                   }
