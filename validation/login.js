cbfc359622457de48ae9cd3dfbf9f3a6ae7acb3a
miker704 <miker704@gmail.com>
2022-07-18 21:30:21 -0400

    changed validation

--- validation/login.js
+++ validation/login.js
@@ -25,7 +25,7 @@ module.exports = function (data) {
   // }
 
   if (Validator.isEmpty(data.password)) {
-    errors.password = "Password field is Required ";
+    errors.password = "Password field is Required";
   }
 
   return {
