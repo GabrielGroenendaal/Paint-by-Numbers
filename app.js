cc3c67dd19d54bf8e084d3e44f8bcff6ad41b228
miker704 <miker704@gmail.com>
2022-07-21 18:03:59 -0400

    how about this?

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const cors = require("cors");
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
+  app.use(cors());
+
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   })
