e5e5fae61e2a9ae18bcc57120211af546e51b532
Gabriel <gabegroenendaal@gmail.com>
2022-07-27 14:52:20 -0400

    Adding LinkedIn Links to footer

--- frontend/src/components/navbar/footer.jsx
+++ frontend/src/components/navbar/footer.jsx
@@ -11,9 +11,9 @@ class footer extends React.Component {
                     {/* <a href="https://github.com/GabrielGroenendaal/Paint-by-Numbers">
                     <i className="fa fa-github"></i>  
                     </a> */}
-                    <label> Gabriel G.</label>
-                    <label> Michael R.</label>
-                    <label> Karen P.</label>
+                    <label> <a href="https://www.linkedin.com/in/gabriel-groenendaal/" target="_blank" rel="noopener referrer">Gabriel G.</a></label>
+                    <label> <a href="https://www.linkedin.com/in/michael-ramoutar/" target="_blank" rel="noopener referrer">Michael R.</a></label>
+                    <label> <a href="https://www.linkedin.com/in/karen-polanco-374721180/" target="_blank" rel="noopener referrer">Karen P.</a></label>
                 </div>
             </div>
         );
